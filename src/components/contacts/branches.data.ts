1578576e94d36ceb4cebe8aed8d8b284fd51ab38
Khasanboy Akhmedov <paragon7400@gmail.com>
2026-01-13 22:30:33 +0500

    fix: maps issue use yandex maps

--- src/components/contacts/branches.data.ts
+++ src/components/contacts/branches.data.ts
@@ -2,7 +2,8 @@ export interface Branch {
   id: string;
   name: string;
   phones: string[];
-  mapEmbedUrl: string; 
+  mapEmbedUrl: string;
+  coordinates: number[]; 
 }
 
 export const branches: Branch[] = [
@@ -10,48 +11,56 @@ export const branches: Branch[] = [
     id: '1', 
     name: 'Bosh ofis', 
     phones: ['+998 74 228-32-12', '+998 78 298-12-35'],
-    mapEmbedUrl: "https://yandex.com/map-widget/v1/?z=16&ol=biz&oid=196915386086"
+    mapEmbedUrl: "https://yandex.com/map-widget/v1/?z=16&ol=biz&oid=196915386086",
+    coordinates: [40.765394, 72.354256],
   },
   { 
     id: '2', 
     name: 'Asaka filiali', 
     phones: ['+998 55 203-35-12'],
-    mapEmbedUrl: "https://yandex.com/map-widget/v1/?..." 
+    mapEmbedUrl: "https://yandex.com/map-widget/v1/?...",
+    coordinates: [40.765394, 72.354256], 
   },
   { 
     id: '3', 
     name: 'Jalaquduq filiali', 
     phones: ['+998 74 755-22-30'],
-    mapEmbedUrl: "https://yandex.com/map-widget/v1/?..." 
+    mapEmbedUrl: "https://yandex.com/map-widget/v1/?...",
+    coordinates: [40.765394, 72.354256],
   },
   { 
     id: '4', 
     name: "Bo'ston (Buloqboshi) filiali",
     phones: ['+998 55 203-12-35'],
-    mapEmbedUrl: "https://yandex.com/map-widget/v1/?..." 
+    mapEmbedUrl: "https://yandex.com/map-widget/v1/?...",
+    coordinates: [40.765394, 72.354256],
   },
   { 
     id: '5', 
     name: "Farg'ona filiali",
     phones: ['+998 55 810-40-20'],
-    mapEmbedUrl: "https://yandex.com/map-widget/v1/?..." 
+    mapEmbedUrl: "https://yandex.com/map-widget/v1/?...",
+    coordinates: [40.765394, 72.354256],
   },
   { 
     id: '6', 
     name: "Furqat filiali",
     phones: ['+998 55 203-32-12'],
-    mapEmbedUrl: "https://yandex.com/map-widget/v1/?..." 
+    mapEmbedUrl: "https://yandex.com/map-widget/v1/?...",
+    coordinates: [40.765394, 72.354256],
   },
   { 
     id: '7', 
     name: "Paxtaobod filiali",
     phones: ['+998 55 202-35-12'],
-    mapEmbedUrl: "https://yandex.com/map-widget/v1/?ll=[COORDINATES]&z=[ZOOM]" 
+    mapEmbedUrl: "https://yandex.com/map-widget/v1/?ll=[COORDINATES]&z=[ZOOM]",
+    coordinates: [40.765394, 72.354256],
   },
   { 
     id: '8', 
     name: "Jahon bozor filiali",
     phones: ['+998 55 201-67-61'],
-    mapEmbedUrl: "https://yandex.com/map-widget/v1/?ll=[COORDINATES]&z=[ZOOM]" 
+    mapEmbedUrl: "https://yandex.com/map-widget/v1/?ll=[COORDINATES]&z=[ZOOM]",
+    coordinates: [41.311151, 69.279737],
   },
 ];
--- src/components/contacts/branches.tsx
+++ src/components/contacts/branches.tsx
@@ -1,49 +1,73 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { FaPhoneAlt } from 'react-icons/fa';
 import './branches.css';
 import { branches } from './branches.data';
+import { Map, Placemark, YMaps, ZoomControl } from '@pbe/react-yandex-maps';
 
 const Branches = () => {
-  const [selectedBranchId, setSelectedBranchId] = useState('1');
+    const [selectedBranchId, setSelectedBranchId] = useState('1');
+    const mapRef = useRef<ymaps.Map | undefined>(undefined);
 
-  const activeBranch = branches.find(b => b.id === selectedBranchId) || branches[0];
+    const activeBranch = branches.find(b => b.id === selectedBranchId) || branches[0];
 
-  return (
-    <div className="bottom-section">
-      <div className="map-container">
-        <iframe 
-            key={activeBranch.id}
-            src={activeBranch.mapEmbedUrl}
-            loading="lazy"
-            className='map'
-            title="Yandex Map"
-        />
-      </div>
+    useEffect(() => {
+        if (mapRef.current) {
+        mapRef.current.panTo(activeBranch.coordinates, {
+            flying: true,
+            duration: 1000,
+        });
+        }
+    }, [activeBranch]);
 
-      <div className="branches-wrapper">
-        <h3 className="branches-title">Telefon raqamlari:</h3>
-        <div className="branches-grid">
-          {branches.map((branch) => (
-            <div 
-              key={branch.id} 
-              className={`branch-card ${selectedBranchId === branch.id ? 'active' : ''}`}
-              onClick={() => setSelectedBranchId(branch.id)}
-            >
-              <h4 className="branch-name">{branch.name}</h4>
-              <div className="branch-phones">
-                {branch.phones.map((phone) => (
-                  <div key={phone} className="phone-item">
-                    <FaPhoneAlt className="phone-icon" />
-                    <span>{phone}</span>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+    return (
+        <div className="bottom-section">
+        <div className="map-container">
+            <YMaps> 
+                <Map 
+                    instanceRef={mapRef}
+                    defaultState={{ 
+                        center: activeBranch.coordinates, 
+                        zoom: 17, 
+                        controls: [] 
+                    }}
+                    className="map" 
+                    width="100%"
+                    height="100%"
+                >
+                    <ZoomControl options={{ position: { right: '10px', top: '10px' } }} />
+                    
+                    <Placemark 
+                        geometry={activeBranch.coordinates} 
+                        options={{ preset: 'islands#redDotIcon' }}
+                    />
+                </Map>
+            </YMaps>
         </div>
-      </div>
-    </div>
-  );
+
+        <div className="branches-wrapper">
+            <h3 className="branches-title">Telefon raqamlari:</h3>
+            <div className="branches-grid">
+            {branches.map((branch) => (
+                <div 
+                key={branch.id} 
+                className={`branch-card ${selectedBranchId === branch.id ? 'active' : ''}`}
+                onClick={() => setSelectedBranchId(branch.id)}
+                >
+                <h4 className="branch-name">{branch.name}</h4>
+                <div className="branch-phones">
+                    {branch.phones.map((phone) => (
+                    <div key={phone} className="phone-item">
+                        <FaPhoneAlt className="phone-icon" />
+                        <span>{phone}</span>
+                    </div>
+                    ))}
+                </div>
+                </div>
+            ))}
+            </div>
+        </div>
+        </div>
+    );
 };
 
 export default Branches;
