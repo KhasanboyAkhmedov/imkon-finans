41a16d0122b6cd98b7da10309b48480fb6a7bd3c
Khasanboy Akhmedov <paragon7400@gmail.com>
2026-02-22 14:43:41 +0500

    fix: date errors in important facts and development strategy pages

--- src/pages/development-strategy/development-strategy.data.ts
+++ src/pages/development-strategy/development-strategy.data.ts
@@ -3,19 +3,11 @@ import documentFile from '/docs/business-plans/lecture.pdf';
 
 export const DevelopmentStrategyData: BusinessFile[] = [
     {
-      id: '1',
+      _id: '1',
       name: `Rivojlanish strategiyasi 2026 yil`,
       size: '412 Kb',
       type: 'PDF',
       fileUrl: documentFile,
-      createdAt: new Date('2025-12-20'),
-    },
-    {
-      id: '2',
-      name: `Rivojlanish strategiyasi 2023 yil`,
-      size: '412 Kb',
-      type: 'PDF',
-      fileUrl: documentFile,
-      createdAt: new Date('2022-12-20'),
+      createdAt: "2026-02-14T09:52:12.659Z",
     },
 ]
