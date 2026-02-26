7cc779a60bf80943b8c75e59c73957eb8df83336
Khasanboy Akhmedov <paragon7400@gmail.com>
2026-02-26 23:06:05 +0500

    multilang: repurchased shares

--- src/pages/re-purchased-shares/re-purchased-shares.tsx
+++ src/pages/re-purchased-shares/re-purchased-shares.tsx
@@ -1,22 +1,25 @@
 import { HiOutlineSearch } from "react-icons/hi"
 import './re-purchased-shares.css'
+import { useTranslation } from "react-i18next";
 
 const RePurchasedShares = () => {
-  return (
-    <section className='re-purchased-shares-section'>
-        <div className='container'>
-            <h2 className="section-title">Qayta sotib olingan aksiyalar</h2>
+    const { t } = useTranslation('pages', { keyPrefix: 're_purchased_shares' });
 
-            <div className="custom-empty-state">
-                <div className="empty-icon-wrapper">
-                    <HiOutlineSearch />
+    return (
+        <section className='re-purchased-shares-section'>
+            <div className='container'>
+                <h2 className="section-title">{t('title')}</h2>
+
+                <div className="custom-empty-state">
+                    <div className="empty-icon-wrapper">
+                        <HiOutlineSearch />
+                    </div>
+                    <h3 className="empty-title">{t('no_repurchased_shares')}</h3>
+                    <p className="empty-description">{t('no_data_to_show')}</p>
                 </div>
-                <h3 className="empty-title">Qayta sotib olingan aksiyalar yo'q</h3>
-                <p className="empty-description">Ko'rsatish uchun ma'lumotlar yo'q</p>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default RePurchasedShares
