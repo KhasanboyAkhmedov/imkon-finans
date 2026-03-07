54fccf88ad4c0d7f1b81c1178b36c8a4cf934df8
Khasanboy Akhmedov <paragon7400@gmail.com>
2026-03-07 12:44:30 +0500

    fix: 404 page translation

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,27 +1,24 @@
+import { useTranslation } from 'react-i18next';
 import './not-found.css';
 
 const NotFound = () => {
+  const { t } = useTranslation('pages', { keyPrefix: 'not_found' });
+  
   return (
     <div className="page-wrapper ">
       <div className="card container">
         <div className="graphic-container">
           <span className="digit">404</span>
-          {/* <div className="gear-icon">
-            <div className="gear-inner"></div>
-          </div>
-          <span className="digit">4</span> */}
         </div>
 
-        {/* Text Section */}
-        <h1 className="status-title">Ups! Bunday sahifa mavjud emas</h1>
-        <p className="status-subtitle">Xatolik yoki kamchilik sezgan bo'lsangiz bizga habar bering</p>
+        <h1 className="status-title">{t('page_not_found')}</h1>
+        <p className="status-subtitle">{t('subtitle')}</p>
 
-        {/* Action Button */}
         <button 
           className="back-button" 
           onClick={() => window.location.href = '/'}
         >
-          Bosh sahifaga qaytish
+          {t('back_home')}
         </button>
       </div>
     </div>
